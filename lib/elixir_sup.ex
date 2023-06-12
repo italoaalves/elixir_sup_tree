ddfd0874ebd77efdc4ff6f43bb855e6487e31bf6
Italo Alves <italoalves.software@gmail.com>
2023-06-11 22:30:24 -0300

    remove: unused code

--- lib/elixir_sup.ex
+++ lib/elixir_sup.ex
@@ -1,18 +1,2 @@
 defmodule ElixirSup do
-  @moduledoc """
-  Documentation for `ElixirSup`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirSup.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/elixir_sup_test.exs
+++ test/elixir_sup_test.exs
@@ -1,8 +1,4 @@
 defmodule ElixirSupTest do
   use ExUnit.Case
   doctest ElixirSup
-
-  test "greets the world" do
-    assert ElixirSup.hello() == :world
-  end
 end
